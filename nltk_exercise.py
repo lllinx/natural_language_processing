c136e318f79160999191b6fbc27af219fca2093a
Lin Xu <lllinx@umich.edu>
2018-03-31 23:50:39 -0500

    basic nltk function

--- nltk_exercise.py
+++ nltk_exercise.py
@@ -13,3 +13,20 @@ text1.generate()
 
 
 ########### Counting Vocabulary###############
+len(text3)
+sorted(set(text3))
+len(set(text3))
+
+# Frequency Distributions
+fdist1 = FreqDist(text1)
+vocabulary1 = list(fdist1.keys())
+vocabulary1[:50]
+fdist1.plot(50, cumulative=True)
+# words that occur once only, the so-called hapaxes
+fdist1.hapaxes()
+# select long words that exists several times 
+fdist5 = FreqDist(text5)
+sorted([w for w in set(text5) if len(w)>7 and fdist5[w]>7]
+
+
+# Collocations and Bigrams
